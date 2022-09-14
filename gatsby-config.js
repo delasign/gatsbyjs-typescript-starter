92f3bd5439dd6d37d14db65e2387b95e7a25635c
Oscar de la Hera Gomez <oscar@delasign.com>
2022-09-14 11:05:24 -0400

    Remove comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,4 @@
 // required to make the environment variables work.
-// This implies .env.development for development & .env.production for production.
 require("dotenv").config({
   path: `.env`,
 });
